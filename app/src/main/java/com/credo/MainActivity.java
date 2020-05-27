129f641992f980d204a4156dc8c269741c1eddbf
mishraaditya595 <mishraaditya595@gmail.com>
2020-05-27 09:55:03 +0530

    Added Google Sign In Feature

--- app/src/main/java/com/credo/MainActivity.java
+++ app/src/main/java/com/credo/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MainActivity.this, LogInActivity.class);
         startActivity(intent);
+
+        findViewById(R.id.signout_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        GoogleSignInAccount signInAccount= GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount!=null)
+        {
+            Toast.makeText(this, signInAccount.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
