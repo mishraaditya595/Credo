025431f86a3ffa3bc235bce3d4c5af073bd6b4fc
mishraaditya595 <mishraaditya595@gmail.com>
2020-05-31 16:54:20 +0530

    Added image compression to load thumbnails faster for users with slower data connection

--- app/src/main/java/com/credo/NewPostActivity.java
+++ app/src/main/java/com/credo/NewPostActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,8 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FieldValue;
@@ -24,10 +27,14 @@ import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -62,39 +69,66 @@ public class NewPostActivity extends AppCompatActivity {
                 {
                     newPostProgressBar.setVisibility(View.VISIBLE);
 
-                    String randomName = UID+"_"+FieldValue.serverTimestamp().toString();
+                    final String randomName = UID+"_"+randomStringGenerator();
                     final StorageReference filePath=storageReference.child("blog_images").child(randomName+".jpg");
                     filePath.putFile(postImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            final String downloadUri=filePath.getDownloadUrl().toString();
+
                             if(task.isSuccessful())
                             {
-                                String downloadUri=filePath.getDownloadUrl().toString();
+                                Bitmap compressedImageFile=null;
 
-                                Map<String, Object> postMap = new HashMap<>();
-                                postMap.put("image_url",downloadUri);
-                                postMap.put("blog_title",blogTitle);
-                                postMap.put("blog_description",blogDescription);
-                                postMap.put("author",UID);
-                                postMap.put("timestamp",FieldValue.serverTimestamp());
+                                try {
+                                    compressedImageFile=compressBlogImageFile();
+                                } catch (IOException e) {
+                                    Toast.makeText(NewPostActivity.this, "Compression Error: "+e.toString(), Toast.LENGTH_SHORT).show();
+                                    return;//so that the program run stops
+                                }
 
-                                firestore.collection("posts").document(UID).set(postMap)
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                if(task.isSuccessful())
-                                                {
-                                                    Toast.makeText(NewPostActivity.this, "Blog published successfully.", Toast.LENGTH_SHORT).show();
-                                                    startActivity(new Intent(NewPostActivity.this,MainActivity.class));
-                                                    finish();
-                                                }
-                                                else
-                                                {
+                                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                                byte[] thumbdata = baos.toByteArray();
+                                final UploadTask thumbnailUploadTask = storageReference.child("blog_images/thumbs").child("thumb_"+randomName+".jpg").putBytes(thumbdata);
 
-                                                }
-                                                newPostProgressBar.setVisibility(View.INVISIBLE );
-                                            }
-                                        });
+                                thumbnailUploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                                        final String downloadThumbUri=thumbnailUploadTask.getResult().getStorage().getDownloadUrl().toString(); //get the download uri here
+
+                                        Map<String, Object> postMap = new HashMap<>();
+                                        postMap.put("image_url",downloadUri);
+                                        postMap.put("thumbnail_url",downloadThumbUri);
+                                        postMap.put("blog_title",blogTitle);
+                                        postMap.put("blog_description",blogDescription);
+                                        postMap.put("author",UID);
+                                        postMap.put("timestamp",FieldValue.serverTimestamp());
+
+                                        firestore.collection("posts").document(UID).collection(blogTitle).document().set(postMap)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(NewPostActivity.this, "Blog published successfully.", Toast.LENGTH_SHORT).show();
+                                                            startActivity(new Intent(NewPostActivity.this, MainActivity.class));
+                                                            finish();
+                                                        } else {
+                                                            String errorMsg = task.getException().getMessage();
+                                                            Toast.makeText(NewPostActivity.this, "Firestore Error: " + errorMsg, Toast.LENGTH_SHORT).show();
+                                                        }
+                                                        newPostProgressBar.setVisibility(View.INVISIBLE );
+
+                                                    }
+                                                });
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(NewPostActivity.this, "Compression Error: "+e.toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
                             }
                             else
                             {
@@ -146,4 +180,24 @@ public class NewPostActivity extends AppCompatActivity {
             }
         }
     }
+
+    private static String randomStringGenerator() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(200);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+    private Bitmap compressBlogImageFile() throws IOException {
+        File blogImageFile=new File(postImageUri.getPath());
+        Bitmap compressedImageFile = new Compressor(NewPostActivity.this)
+                .setQuality(5).compressToBitmap(blogImageFile);
+        return compressedImageFile;
+    }
+
 }
