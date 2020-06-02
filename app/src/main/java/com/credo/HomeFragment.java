0c5d166487829ba6981022f1c564eeb427234ed3
mishraaditya595 <mishraaditya595@gmail.com>
2020-06-02 22:07:47 +0530

    Added Pagination Features

--- app/src/main/java/com/credo/HomeFragment.java
+++ app/src/main/java/com/credo/HomeFragment.java
@@ -2,6 +2,7 @@ package com.credo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,10 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -27,6 +31,7 @@ public class HomeFragment extends Fragment {
     private List<BlogPost> blogPostList;
     private FirebaseFirestore firestore;
     private BlogRecyclerAdapter blogRecyclerAdapter;
+    private DocumentSnapshot lastVisible;
 
     public HomeFragment() {
     }
@@ -40,21 +45,44 @@ public class HomeFragment extends Fragment {
 
         initialiseFields(view);
 
-        firestore.collection("posts").addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                assert queryDocumentSnapshots != null;
-                for(DocumentChange doc: queryDocumentSnapshots.getDocumentChanges())
-                {
-                    if(doc.getType() == DocumentChange.Type.ADDED)
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null)
+        {
+            homeFragmentRV.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+
+                    Boolean reachedBottom=!recyclerView.canScrollVertically(1);
+                    if(reachedBottom)
                     {
-                        BlogPost blogPost=doc.getDocument().toObject(BlogPost.class);
-                        blogPostList.add(blogPost);
-                        blogRecyclerAdapter.notifyDataSetChanged();
+                        loadMorePosts();
                     }
                 }
-            }
-        });
+            });
+
+            //the following line is to make the newest posts appear first and only 4 posts will be loaded at a time
+            Query firstQuery=firestore.collection("posts").orderBy("timestamp", Query.Direction.DESCENDING).limit(4);
+
+            firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                    lastVisible=queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size()-1);
+
+                    assert queryDocumentSnapshots != null;
+                    for(DocumentChange doc: queryDocumentSnapshots.getDocumentChanges())
+                    {
+                        if(doc.getType() == DocumentChange.Type.ADDED)
+                        {
+                            BlogPost blogPost=doc.getDocument().toObject(BlogPost.class);
+                            blogPostList.add(blogPost);
+                            blogRecyclerAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            });
+
+        }
 
         return view;
     }
@@ -66,5 +94,37 @@ public class HomeFragment extends Fragment {
         blogRecyclerAdapter=new BlogRecyclerAdapter(blogPostList);
         homeFragmentRV.setLayoutManager(new LinearLayoutManager(getActivity()));
         homeFragmentRV.setAdapter(blogRecyclerAdapter);
+    }
+
+    public void loadMorePosts(){
+        //the following line is to make the newest posts appear first and only 4 posts will be loaded at a time
+        Query nextQuery=firestore.collection("posts")
+                .orderBy("timestamp", Query.Direction.DESCENDING)
+                .startAfter(lastVisible)
+                .limit(4);
+
+        nextQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if(!queryDocumentSnapshots.isEmpty())
+                {
+                    //we will only load more posts if they exist else we will not
+
+                    lastVisible=queryDocumentSnapshots.getDocuments().get(queryDocumentSnapshots.size()-1);
+
+                    assert queryDocumentSnapshots != null;
+                    for(DocumentChange doc: queryDocumentSnapshots.getDocumentChanges())
+                    {
+                        if(doc.getType() == DocumentChange.Type.ADDED)
+                        {
+                            BlogPost blogPost=doc.getDocument().toObject(BlogPost.class);
+                            blogPostList.add(blogPost);
+                            blogRecyclerAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        });
     }
 }
